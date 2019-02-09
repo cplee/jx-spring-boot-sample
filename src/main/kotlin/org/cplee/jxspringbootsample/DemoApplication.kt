e2b1a59f0eb5a02dec4bfd5f2a22ddb048aae44e
Casey Lee <casey@liatrio.com>
2019-02-08 16:43:15 -0800

    Initial import

---
+++ src/main/kotlin/org/cplee/jxspringbootsample/DemoApplication.kt
@@ -0,0 +1,12 @@
+package org.cplee.jxspringbootsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
---
+++ src/test/kotlin/org/cplee/jxspringbootsample/DemoApplicationTests.kt
@@ -0,0 +1,17 @@
+package org.cplee.jxspringbootsample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
